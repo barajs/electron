6ca2874bbea79de608bc44971a0e3c6815425245
nampdn <nampdn@protonmail.com>
2019-10-24 16:18:07 +0700

    fix(seep): remove console log

--- src/seep/window.ts
+++ src/seep/window.ts
@@ -1,13 +1,4 @@
 import { WindowProps } from '../flow'
 
-export const winNameEq = (name: string) => (payload: WindowProps) => {
-  console.log(
-    `Checking win name ${name} :${JSON.stringify(
-      payload.win.__metadata,
-      null,
-      2,
-    )}`,
-    payload.win.__metadata.name === name || false,
-  )
-  return payload.win.__metadata.name === name || false
-}
+export const winNameEq = (name: string) => (payload: WindowProps) =>
+  payload.win.__metadata.name === name || false
